250b4acd3b20ffa707596deb51820f6db83c1399
Darion Hall <darionhall@gmail.com>
2016-04-23 20:18:19 -0400

    Added MVC CRUD for User

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,4 +1,7 @@
 class Card < ActiveRecord::Base
+
+  belongs_to :user
+
   validates :name presence: true
   validates :profession presence: true
   validates :email presence: true
--- db/migrate/20160423230840_create_cards.rb
+++ db/migrate/20160423230840_create_cards.rb
@@ -2,13 +2,15 @@ class CreateCards < ActiveRecord::Migration
   def change
     create_table :cards do |t|
       t.string :name, null: false
-      t.string :profession null: false
+      t.string :profession, null: false
       t.string :email, null: false
+      #email should be :string:uniq
       t.integer :phone, null: false
       t.string :websites
       t.string :skills
 
       t.timestamps null: false
     end
+    add_index :cards, :email, unique: true
   end
 end

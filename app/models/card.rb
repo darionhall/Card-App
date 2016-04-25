bcc78134ff0b1b83884963342611709c7dfc0536
Darion Hall <darionhall@gmail.com>
2016-04-25 16:02:44 -0400

    Fixing validation in card.rb

--- app/models/card.rb
+++ app/models/card.rb
@@ -2,12 +2,9 @@ class Card < ActiveRecord::Base
 
   belongs_to :user
 
-  validates :name, presence: true
-  validates :profession, presence: true
-  validates :email, presence: true
-  validates :phone, presence: true
-  validates :websites
-  validates :skills
+  validates :name, :profession, :email, :phone presence: true
+  # validates :websites
+  # validates :skills
 
   before_save :default_values
 
